10a06b2a8960e4e0f68bb5d1945171cb37fa3bc7
Natan Cordeiro <o.natan1907@gmail.com>
2022-08-24 18:47:40 -0300

    aula sobre groupby

---
+++ intermediario/aula11.py
@@ -0,0 +1,27 @@
+"""
+ Groupby - agrupando os valores
+"""
+from itertools import groupby
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Leticia', 'nota': 'B'},
+    {'nome': 'Fabricio', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Juana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+    {'nome': 'José', 'nota': 'B'}
+]
+#   função lambda para ordenar por nota
+ordena = lambda item: item['nota']
+alunos.sort(key=ordena)
+alunos_agrupados = groupby(alunos, ordena)
+
+for agrupamento, valores_agrupados in alunos_agrupados:
+    print(f'Agrupamento: {agrupamento}')
+
+    quantidade = len(list(valores_agrupados))
+    print(f'{quantidade} alunos tiraram a nota {agrupamento}')
