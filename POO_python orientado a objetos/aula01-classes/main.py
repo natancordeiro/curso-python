01fa29b6449329aee200dab60f77015dd943816e
Natan Cordeiro <o.natan1907@gmail.com>
2022-08-30 15:09:12 -0300

    orientado a objetos

---
+++ POO_python orientado a objetos/aula01-classes/main.py
@@ -0,0 +1,8 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Natan', 22)
+p2 = Pessoa('Rafaela', 29)
+
+p1.falar('POO')
+p2.comer('sorvete')
+p1.comer('churrasco')
---
+++ POO_python orientado a objetos/aula01-classes/pessoa.py
@@ -0,0 +1,53 @@
+"""
+ Classes em Python
+"""
+class Pessoa:
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo.')
+            return
+
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+
+        print(f'{self.nome} está falando sobre {assunto}.')
+        self.falando = True
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não está falando.')
+            return
+
+        if self.falando:
+            print(f'{self.nome} não pode comer falando.')
+            return
+
+        print(f'{self.nome} parou de falar.')
+        self.flanado = False
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo.')
+            return
+
+        if self.falando:
+            print(f'{self.nome} não pode comer falando.')
+            return
+
+        print(f'{self.nome} está comendo {alimento}.')
+        self.comendo = True
+
+    def para_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não stá comendo.')
+            return
+
+        print(f'{self.nome} parou de comer.')
+        self.comendo = False
