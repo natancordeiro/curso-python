79607ca3376aaf379d26d7ceabbdfe09fd2f7e3b
Natan Cordeiro <o.natan1907@gmail.com>
2022-08-30 12:58:57 -0300

    finalizei projeto de uma "To-Do List"

--- intermediario/projetos/lista_tarefas.py
+++ intermediario/projetos/lista_tarefas.py
@@ -1,17 +1,11 @@
-"""
-Faça uma lista de tarefas com as seguintes opções:
-    adicionar tarefa
-    listar tarefas
-    opção de desfazer (A cada vez que chamarmos, desfaz a ultima tarefa)
-    opção de refazer (A cada vez que chamarmos, refaz a ultima tarefa)
-"""
-
+# Função para MOSTRAR as oções cadastradas
 def show_op(todo_list):
     print()
     print('Tarefas: ')
     print(todo_list)
     print()
 
+# Função para DESFAZER
 def do_undo(todo_list, redo_list):
     if not todo_list: 
         print('Nada a desfazer.')
@@ -19,23 +13,33 @@ def do_undo(todo_list, redo_list):
     last_todo = todo_list.pop()
     redo_list.append(last_todo)
 
+# Função para REFAZER
 def do_redo(todo_list, redo_list):
     if not redo_list:
         print('Nada a refazer.')
         return
-    
     last_redo = redo_list.pop()
     todo_list.append(last_redo)
 
+# Função para ADICIONAR objeto ao todo_list 
 def do_add(todo, todo_list):
     todo_list.append(todo)
 
+# Tratamento de Módulo
 if __name__ == 'main':
     todo_list = []
     redo_list = []
 
+    # Looping de input do usuário
     while True:
-        todo = input('Digite uma tarefa ou undo, redo, ls: ')
+        todo = input('''
+        Digite a tarefa que deseja fazer: 
+        Outras opções:  
+        [undo] para desfazer
+        [redo] para refazer
+        [ls] para listar ''')
+
+        # Tratando a escolha do usuário
         if todo == 'ls':
             show_op(todo_list)
             continue
