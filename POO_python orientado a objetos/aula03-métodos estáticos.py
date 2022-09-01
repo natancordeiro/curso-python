8272026ea05e5cf5cf29a4d5cba28680516ad4f3
Natan Cordeiro <o.natan1907@gmail.com>
2022-08-31 21:31:38 -0300

    métodos estáticos

---
+++ POO_python orientado a objetos/aula03-métodos estáticos.py
@@ -0,0 +1,30 @@
+from random import randint
+
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        print(self.ano_atual - self.idade)
+
+@classmethod
+def por_ano_nascimento(cls, nome, ano_nascimento):
+    idade = cls.ano_atual - ano_nascimento
+    return cls(nome, idade)
+
+@staticmethod
+def gera_id():
+    rand = randint(10000, 19999)
+    return rand
+
+#-------------------------------------
+
+p1 = Pessoa('Natan', 22)
+print(p1)
+print(p1.nome, p1.idade)
+p1.get_ano_nascimento()
+
+print(gera_id())
