2f6094c5633dc0552094e606a5a762e96b96326d
Natan Cordeiro <o.natan1907@gmail.com>
2022-09-11 17:54:18 -0300

    sobreposição de membros

--- POO_python orientado a objetos/aula10-herança/classes.py
+++ POO_python orientado a objetos/aula10-herança/classes.py
@@ -17,3 +17,19 @@ class Aluno(Pessoa):
     # Função que só funciona em seua subclasse
     def estudar(self):
         print(f'{self.nomeclasse} Estudando...')  
+
+class ClienteVIP(Cliente):
+    def falar(self):
+        super().falar() # se refere a função da classe principal 
+        print('Outra coisa qualquer.')
+        # Sobreposição / Reescrever o que o método faz
+    def __init__(self, nome, idade, sobrenome):
+        super().__init__(nome, idade)
+        self.sobrenome = sobrenome
+        # Reescrevendo o método construtor 
+
+    def falar(self):
+        Pessoa.falar(self)
+        Cliente.falar(self)
+        print(f'{self.nome} {self.sobrenome}')
+    
--- POO_python orientado a objetos/aula10-herança/main.py
+++ POO_python orientado a objetos/aula10-herança/main.py
@@ -8,3 +8,6 @@ c1.falar()
 
 a1 = Aluno('Rafa', 29)
 a1.falar()
+
+c2 = ClienteVIP('Natan', 22, 'Cordeiro')
+c2.falar()
